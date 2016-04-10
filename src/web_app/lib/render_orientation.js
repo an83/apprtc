5bded84c1d5379b6c229ba90bd13f775b24387cb
Alaeddin Nassani <anassani@jadeworld.com>
2016-04-10 20:39:40 +1200

    auto-scroll to bottom

--- src/web_app/lib/render_orientation.js
+++ src/web_app/lib/render_orientation.js
@@ -188,7 +188,10 @@ SceneController.prototype.addText = function (font, text, x, y, z, color) {
 
 SceneController.prototype.addAnnotation = function (annotation) {
     var $history = jQuery('#annotation-history');
-    jQuery('<div />', {'class': 'history-item', 'style': 'color: ' + annotation.color}).text(annotation.text).appendTo($history);
+    jQuery('<div />', {'class': 'history-item', 'style': 'color: ' + annotation.color}).text(annotation.text)
+        .appendTo($history);
+
+    $history.scrollTop($history.prop("scrollHeight"));
 };
 
 SceneController.prototype.addTag = function (text, x, y, z, color) {
